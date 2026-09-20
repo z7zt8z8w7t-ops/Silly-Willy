1f05dbb85ae5eb9564cba77c83f6ebcc92eae6fe
z7zt8z8w7t-ops <z7zt8z8w7t@privaterelay.appleid.com>
2026-09-20 13:31:34 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='silly-willy-v1';
+const CACHE='silly-willy-v2';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
